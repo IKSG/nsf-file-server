f584d3a3923d65c29f89996ddf169cf6c2d86321
Jesse Gallagher <jesse@secondfoundation.org>
2020-03-04 11:04:40 -0500

    Restructure module layout and add Domino app

---
+++ nsf-file-server/core/nsf-file-server-ssh/src/main/java/org/openntf/nsffile/ssh/SshServerDelegate.java
@@ -0,0 +1,88 @@
+/**
+ * Copyright © 2019-2020 Jesse Gallagher
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openntf.nsffile.ssh;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Collections;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.apache.sshd.common.file.FileSystemFactory;
+import org.apache.sshd.server.SshServer;
+import org.apache.sshd.server.keyprovider.SimpleGeneratorHostKeyProvider;
+import org.apache.sshd.server.scp.ScpCommandFactory;
+import org.apache.sshd.server.subsystem.sftp.SftpSubsystemFactory;
+import org.openntf.nsffile.ssh.auth.NotesPasswordAuthenticator;
+import org.openntf.nsffile.ssh.auth.NotesPublicKeyAuthenticator;
+import org.openntf.nsffile.ssh.scp.NSFScpFileOpener;
+
+/**
+ * Frontend-independent manager for running the SSH/SFTP server.
+ * 
+ * @author Jesse Gallagher
+ * @since 1.0.0
+ */
+public class SshServerDelegate implements AutoCloseable {
+	public static final Logger log = Logger.getLogger(SshServerDelegate.class.getPackage().getName());
+	
+	private final String nsfPath;
+	private final int port;
+	private final Path keyPath;
+	private final FileSystemFactory fileSystemFactory;
+	
+	private SshServer server;
+
+	public SshServerDelegate(String nsfPath, int port, Path keyPath, FileSystemFactory fileSystemFactory) {
+		this.nsfPath = nsfPath;
+		this.port = port;
+		this.keyPath = keyPath;
+		this.fileSystemFactory = fileSystemFactory;
+	}
+	
+	public void start() throws IOException {
+		if(log.isLoggable(Level.INFO)) {
+			log.info(getClass().getSimpleName() + ": Using NSF path " + nsfPath);
+			log.info(getClass().getSimpleName() + ": Using port " + port);
+			log.info(getClass().getSimpleName() + ": Using key path " + keyPath);
+		}
+		
+		server = SshServer.setUpDefaultServer();
+		server.setPort(port);
+		server.setKeyPairProvider(new SimpleGeneratorHostKeyProvider(keyPath));
+		server.setPasswordAuthenticator(new NotesPasswordAuthenticator());
+		server.setPublickeyAuthenticator(new NotesPublicKeyAuthenticator());
+		server.setFileSystemFactory(fileSystemFactory);
+		
+		SftpSubsystemFactory sftp = new SftpSubsystemFactory.Builder()
+			.build();
+		server.setSubsystemFactories(Collections.singletonList(sftp));
+		
+		ScpCommandFactory scp = new ScpCommandFactory.Builder()
+			.withFileOpener(new NSFScpFileOpener(nsfPath))
+			.build();
+		server.setCommandFactory(scp);
+		
+		server.start();
+	}
+	
+	@Override
+	public void close() throws IOException {
+		if(server != null) {
+			server.close();
+		}
+	}
+}
---
+++ nsf-file-server/domino/nsf-file-server-domino/src/main/java/org/openntf/nsffile/domino/SFTPService.java
@@ -0,0 +1,99 @@
+/**
+ * Copyright © 2019-2020 Jesse Gallagher
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openntf.nsffile.domino;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+import javax.servlet.ServletException;
+
+import org.openntf.nsffile.domino.config.DominoNSFConfiguration;
+import org.openntf.nsffile.ssh.SshServerDelegate;
+import org.openntf.nsffile.util.NotesThreadFactory;
+
+import com.ibm.designer.runtime.domino.adapter.ComponentModule;
+import com.ibm.designer.runtime.domino.adapter.HttpService;
+import com.ibm.designer.runtime.domino.adapter.LCDEnvironment;
+import com.ibm.designer.runtime.domino.bootstrap.adapter.HttpServletRequestAdapter;
+import com.ibm.designer.runtime.domino.bootstrap.adapter.HttpServletResponseAdapter;
+import com.ibm.designer.runtime.domino.bootstrap.adapter.HttpSessionAdapter;
+import com.ibm.domino.napi.c.Os;
+
+/**
+ * @author Jesse Gallagher
+ * @since 1.0.0
+ */
+public class SFTPService extends HttpService {
+	public static final String ENV_PORT = "SFTPNSFPort"; //$NON-NLS-1$
+	public static final int DEFAULT_PORT = 9022;
+	
+	private SshServerDelegate server;
+
+	public SFTPService(LCDEnvironment env) {
+		super(env);
+		
+		// Kick off initialization on a separate thread to not block HTTP startup
+		NotesThreadFactory.executor.submit(() -> {
+			try {
+				String dataDir = Os.OSGetEnvironmentString("Directory"); //$NON-NLS-1$
+				Path keyPath = Paths.get(dataDir, getClass().getPackage().getName() + ".keys"); //$NON-NLS-1$
+		
+				String nsfPath = DominoNSFConfiguration.instance.getNsfPath();
+				int port = DominoNSFConfiguration.instance.getPort();
+				this.server = new SshServerDelegate(nsfPath, port, keyPath, session -> DominoNSFConfiguration.instance.getFileSystem(session.getUsername()));
+				
+				server.start();
+			} catch(Throwable t) {
+				t.printStackTrace();
+				server = null;
+			}
+		});
+	}
+	
+	@Override
+	public void destroyService() {
+		super.destroyService();
+		
+		if(server != null) {
+			try {
+				server.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		NotesThreadFactory.term();
+	}
+	
+	// *******************************************************************************
+	// * Inapplicable methods
+	// *******************************************************************************
+
+	@Override
+	public boolean doService(String arg0, String arg1, HttpSessionAdapter arg2, HttpServletRequestAdapter arg3,
+			HttpServletResponseAdapter arg4) throws ServletException, IOException {
+		// NOP
+		return false;
+	}
+
+	@Override
+	public void getModules(List<ComponentModule> modules) {
+		// NOP
+	}
+
+}
