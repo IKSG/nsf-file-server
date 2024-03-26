1aec8b719d7665c6ed065fcb66c156d959a5ce75
Jesse Gallagher <jesse@secondfoundation.org>
2024-03-26 18:31:27 -0400

    Remove unused utility methods

--- nsf-file-server/core/nsf-file-server-fs-nsffilestore/src/main/java/org/openntf/nsffile/fs/db/NSFAccessor.java
+++ nsf-file-server/core/nsf-file-server-fs-nsffilestore/src/main/java/org/openntf/nsffile/fs/db/NSFAccessor.java
@@ -15,7 +15,19 @@
  */
 package org.openntf.nsffile.fs.db;
 
-import static org.openntf.nsffile.fs.NSFFileSystemConstants.*;
+import static org.openntf.nsffile.fs.NSFFileSystemConstants.DATATYPE_NAME;
+import static org.openntf.nsffile.fs.NSFFileSystemConstants.FORM_FOLDER;
+import static org.openntf.nsffile.fs.NSFFileSystemConstants.ITEM_CREATED;
+import static org.openntf.nsffile.fs.NSFFileSystemConstants.ITEM_FILE;
+import static org.openntf.nsffile.fs.NSFFileSystemConstants.ITEM_GROUP;
+import static org.openntf.nsffile.fs.NSFFileSystemConstants.ITEM_MODIFIED;
+import static org.openntf.nsffile.fs.NSFFileSystemConstants.ITEM_OWNER;
+import static org.openntf.nsffile.fs.NSFFileSystemConstants.ITEM_PARENT;
+import static org.openntf.nsffile.fs.NSFFileSystemConstants.ITEM_PERMISSIONS;
+import static org.openntf.nsffile.fs.NSFFileSystemConstants.PREFIX_USERITEM;
+import static org.openntf.nsffile.fs.NSFFileSystemConstants.VIEW_FILESBYPARENT;
+import static org.openntf.nsffile.fs.NSFFileSystemConstants.VIEW_FILESBYPARENT_INDEX_NAME;
+import static org.openntf.nsffile.fs.NSFFileSystemConstants.VIEW_FILESBYPATH;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -42,25 +54,20 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
-import javax.naming.InvalidNameException;
-import javax.naming.ldap.LdapName;
+import com.ibm.commons.util.StringUtil;
+import com.ibm.designer.domino.napi.NotesConstants;
 
 import org.openntf.nsffile.commons.NotesPrincipal;
 import org.openntf.nsffile.commons.fs.attribute.NSFFileAttributes;
 import org.openntf.nsffile.commons.fs.attribute.NSFFileAttributes.Type;
-import org.openntf.nsffile.commons.util.NotesThreadFactory;
 import org.openntf.nsffile.fs.NSFPath;
 import org.openntf.nsffile.fs.util.NSFPathUtil;
-
-import com.ibm.commons.util.StringUtil;
-import com.ibm.designer.domino.napi.NotesConstants;
 
 import lotus.domino.Database;
 import lotus.domino.DateTime;
 import lotus.domino.Document;
 import lotus.domino.EmbeddedObject;
 import lotus.domino.Item;
-import lotus.domino.Name;
 import lotus.domino.NotesException;
 import lotus.domino.RichTextItem;
 import lotus.domino.Session;
@@ -356,105 +363,6 @@ public enum NSFAccessor {
 				}
 			}
 		});
-	}
-	
-	/**
-	 * <p>Takes an Domino-format name and converts it to LDAP format.</p>
-	 * 
-	 * <p>If the provided value is not a valid Domino name, the original value is returned.</p>
-	 */
-	public static String dominoNameToLdap(String value) {
-		// There's not a convenient class handy for this
-		// TODO see if the ODA stuff can be co-opted
-		try {
-			if(StringUtil.isEmpty(value)) {
-				return value;
-			} else if(!value.contains("/")) { //$NON-NLS-1$
-				if(!value.contains("=")) { //$NON-NLS-1$
-					return "cn=" + value; //$NON-NLS-1$
-				} else {
-					// Then it should be an LDAP-type name already
-					return value;
-				}
-			}
-			return NotesThreadFactory.call(session -> {
-				Name name = session.createName(value);
-				try {
-					String dn = name.getCanonical();
-					if(!dn.contains("=")) { //$NON-NLS-1$
-						return dn;
-					}
-					StringBuilder result = new StringBuilder();
-					for(String component : dn.split("/")) { //$NON-NLS-1$
-						if(result.length() > 0) {
-							result.append(',');
-						}
-						int indexEq = component == null ? -1 : component.indexOf('=');
-						if(component != null && indexEq > -1) {
-							result.append(component.substring(0, indexEq).toLowerCase());
-							result.append('=');
-							result.append(component.substring(indexEq+1));
-						} else {
-							result.append(component);
-						}
-					}
-					return result.toString();
-				} finally {
-					name.recycle();
-				}
-			});
-		} catch(Exception e) {
-			if(log.isLoggable(Level.SEVERE)) {
-				log.log(Level.SEVERE, MessageFormat.format("Encountered exception converting Domino name to LDAP: {0}", value), e);
-			}
-			throw new RuntimeException(e);
-		}
-	}
-	
-	/**
-	 * <p>Takes an LDAP-format distinguished name and converts it to Domino format.</p>
-	 * 
-	 * <p>If the provided value is not a valid LDAP name, the original value is returned.</p>
-	 */
-	public static String ldapNameToDomino(String value) {
-		if(StringUtil.isEmpty(value)) {
-			return ""; //$NON-NLS-1$
-		} else {
-			// Make sure it's actually an LDAP name. We'll assume that an un-escaped slash is indicative of a Domino name
-			int slashIndex = value.indexOf('/');
-			while(slashIndex > -1) {
-				if(slashIndex == 0 || value.charAt(slashIndex-1) != '\\') {
-					// Then it's probably a Domino name
-					return value;
-				}
-				slashIndex = value.indexOf('/', slashIndex+1);
-			}
-			
-			try {
-				LdapName dn = new LdapName(value);
-				StringBuilder result = new StringBuilder();
-				// LdapName lists components in increasing-specificity order
-				for(int i = dn.size()-1; i >= 0; i--) {
-					if(result.length() > 0) {
-						result.append("/"); //$NON-NLS-1$
-					}
-					
-					String component = dn.get(i);
-					// Domino likes the component name capitalized - probably not REQUIRED, but it shouldn't hurt
-					int indexEq = component == null ? -1 : component.indexOf('=');
-					if(component != null && indexEq > -1) {
-						result.append(component.substring(0, indexEq).toUpperCase());
-						result.append('=');
-						result.append(component.substring(indexEq+1));
-					} else {
-						result.append(component);
-					}
-				}
-				return result.toString();
-			} catch(InvalidNameException e) {
-				throw new RuntimeException(e);
-			}
-		}
 	}
 	
 	public static NSFFileAttributes readAttributes(NSFPath path) {
